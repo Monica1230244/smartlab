7d3a93064b1ef8f1eb472a0894604bbd26e41b0f
Monica1230244 <monyevefo@gmail.com>
2026-07-07 15:50:49 +0100

    Add modular dashboard mockups

--- frontend/public/sw.js
+++ frontend/public/sw.js
@@ -1,4 +1,4 @@
-﻿const CACHE_NAME = 'smartlab-pwa-v20260706-03';
+const CACHE_NAME = 'smartlab-pwa-v20260707-01';
 const APP_SHELL = ['./', './index.html', './manifest.json'];
 
 self.addEventListener('install', (event) => {
